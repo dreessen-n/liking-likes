70c7e1fcda454dd8a9e01e51e4a9e826eb6ce1e7
dreessen-n <dreessen.edu@gmail.com>
2022-05-15 17:58:35 -0600

    Finish addLike function

---
+++ script.js
@@ -0,0 +1,24 @@
+// Increase likes
+
+function addLike2() {
+    var x = document.querySelector('.like-num1')
+    console.log(x)
+    var num = x.innerText;
+    num++;
+    x.innerText = num;
+    console.log(num);
+}
+
+
+function addLike(element) {
+    // console.log(element);
+    var x = element.id;
+    // console.log(x);
+    var y = 'label_' + x;
+    // console.log(y);
+    var z = document.querySelector('#' + y);
+    // console.log(z);
+    var num = z.innerText;
+    num++;
+    z.innerText = num;
+}
