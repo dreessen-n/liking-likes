f74301cf69061865ad39b3a4435063e1e7dfcf80
dreessen-n <dreessen.edu@gmail.com>
2022-05-15 18:13:35 -0600

    Created addAvatar function to change img

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 // Increase likes
 
-// function addLike2() {
-//     var x = document.querySelector('.like-num1')
-//     console.log(x)
-//     var num = x.innerText;
-//     num++;
-//     x.innerText = num;
-//     console.log(num);
-// }
-
-
 function addLike(element) {
     // console.log(element);
     var x = element.id;
@@ -21,4 +11,10 @@ function addLike(element) {
     var num = z.innerText;
     num++;
     z.innerText = num;
+}
+
+function addAvatar () {
+    var x = document.querySelector('.img_profile-pic');
+    console.log(x);
+    x.src = "./assets/toshiro.png";
 }
